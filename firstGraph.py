505af0ff83dc695de9e1431e54fb513ed926aabd
csnow <colinmsnow@gmail.com>
2021-12-06 15:30:36 -0500

    simple example

--- firstGraph.py
+++ firstGraph.py
@@ -29,9 +29,6 @@ class DiscreteNewGraph(Scene):
         x_2_label = axes.get_graph_label(x_2, "x^2")
         x_4_label = axes.get_graph_label(x_4, "(x-1)^2 (x+1)^2")
 
-        # x_4_label.shift(RIGHT*5 + UP*3)
-
-        # x_4_label.shift(RIGHT)
 
         dot = Dot(color=RED)
         dot.move_to(axes.c2p(0, 0))
@@ -55,14 +52,6 @@ class DiscreteNewGraph(Scene):
 
         x_2_label.shift(RIGHT)
 
-        # def move_dots(dots):
-        #     for dot in dots:
-        #         x = dot[0]
-        #         dot[1].generate_target(use_deepcopy=True)
-        #         dot[1].target = axes.c2p(x, (x-1)*(x-1)*(x+1)*(x+1))
-
-
-        # move_dots(dots)
 
         dots2 = []
         dot_group2 = VGroup()
@@ -75,95 +64,10 @@ class DiscreteNewGraph(Scene):
 
         
         self.play(
-            # dot.animate.move_to(axes.c2p(0,1)),
             ReplacementTransform(x_2, x_4),
             FadeOut(x_2_label),
             FadeIn(x_4_label),
-            ReplacementTransform(dot_group, dot_group2),)
-            # MoveToTarget(dot_group))
-        
-        
-        # self.wait(2)
-
-
-        
-
-        # x_4_label.shift(RIGHT)
-
-                    #FadeTransform(x_2_label, x_4_label, shift = DOWN),
-
-
-        # # Axes.get_graph will return the graph of a function
-        # sin_graph = axes.get_graph(
-        #     lambda x: 2 * math.sin(x),
-        #     color=BLUE,
-        # )
-        # # By default, it draws it so as to somewhat smoothly interpolate
-        # # between sampled points (x, f(x)).  If the graph is meant to have
-        # # a corner, though, you can set use_smoothing to False
-        # relu_graph = axes.get_graph(
-        #     lambda x: max(x, 0),
-        #     use_smoothing=False,
-        #     color=YELLOW,
-        # )
-        # # For discontinuous functions, you can specify the point of
-        # # discontinuity so that it does not try to draw over the gap.
-        # step_graph = axes.get_graph(
-        #     lambda x: 2.0 if x > 3 else 1.0,
-        #     discontinuities=[3],
-        #     color=GREEN,
-        # )
-
-        # # Axes.get_graph_label takes in either a string or a mobject.
-        # # If it's a string, it treats it as a LaTeX expression.  By default
-        # # it places the label next to the graph near the right side, and
-        # # has it match the color of the graph
-        # sin_label = axes.get_graph_label(sin_graph, "\\sin(x)")
-        # relu_label = axes.get_graph_label(relu_graph, Text("ReLU"))
-        # step_label = axes.get_graph_label(step_graph, Text("Step"), x=4)
-
-        # self.play(
-        #     ShowCreation(sin_graph),
-        #     FadeIn(sin_label, RIGHT),
-        # )
-        # self.wait(2)
-        # self.play(
-        #     ReplacementTransform(sin_graph, relu_graph),
-        #     FadeTransform(sin_label, relu_label),
-        # )
-        # self.wait()
-        # self.play(
-        #     ReplacementTransform(relu_graph, step_graph),
-        #     FadeTransform(relu_label, step_label),
-        # )
-        # self.wait()
-
-        # parabola = axes.get_graph(lambda x: 0.25 * x**2)
-        # parabola.set_stroke(BLUE)
-        # self.play(
-        #     FadeOut(step_graph),
-        #     FadeOut(step_label),
-        #     ShowCreation(parabola)
-        # )
-        # self.wait()
-
-        # # You can use axes.input_to_graph_point, abbreviated
-        # # to axes.i2gp, to find a particular point on a graph
-        # dot = Dot(color=RED)
-        # dot.move_to(axes.i2gp(2, parabola))
-        # self.play(FadeIn(dot, scale=0.5))
-
-        # # A value tracker lets us animate a parameter, usually
-        # # with the intent of having other mobjects update based
-        # # on the parameter
-        # x_tracker = ValueTracker(2)
-        # f_always(
-        #     dot.move_to,
-        #     lambda: axes.i2gp(x_tracker.get_value(), parabola)
-        # )
-
-        # self.play(x_tracker.animate.set_value(4), run_time=3)
-        # self.play(x_tracker.animate.set_value(-2), run_time=3)
+            ReplacementTransform(dot_group, dot_group2))
         self.wait()
 
 
@@ -205,82 +109,6 @@ class NewGraph(Scene):
             FadeOut(x_2_label),
             FadeIn(x_4_label),
         )
-        # x_4_label.shift(RIGHT)
-
-                    #FadeTransform(x_2_label, x_4_label, shift = DOWN),
-
-
-        # # Axes.get_graph will return the graph of a function
-        # sin_graph = axes.get_graph(
-        #     lambda x: 2 * math.sin(x),
-        #     color=BLUE,
-        # )
-        # # By default, it draws it so as to somewhat smoothly interpolate
-        # # between sampled points (x, f(x)).  If the graph is meant to have
-        # # a corner, though, you can set use_smoothing to False
-        # relu_graph = axes.get_graph(
-        #     lambda x: max(x, 0),
-        #     use_smoothing=False,
-        #     color=YELLOW,
-        # )
-        # # For discontinuous functions, you can specify the point of
-        # # discontinuity so that it does not try to draw over the gap.
-        # step_graph = axes.get_graph(
-        #     lambda x: 2.0 if x > 3 else 1.0,
-        #     discontinuities=[3],
-        #     color=GREEN,
-        # )
-
-        # # Axes.get_graph_label takes in either a string or a mobject.
-        # # If it's a string, it treats it as a LaTeX expression.  By default
-        # # it places the label next to the graph near the right side, and
-        # # has it match the color of the graph
-        # sin_label = axes.get_graph_label(sin_graph, "\\sin(x)")
-        # relu_label = axes.get_graph_label(relu_graph, Text("ReLU"))
-        # step_label = axes.get_graph_label(step_graph, Text("Step"), x=4)
-
-        # self.play(
-        #     ShowCreation(sin_graph),
-        #     FadeIn(sin_label, RIGHT),
-        # )
-        # self.wait(2)
-        # self.play(
-        #     ReplacementTransform(sin_graph, relu_graph),
-        #     FadeTransform(sin_label, relu_label),
-        # )
-        # self.wait()
-        # self.play(
-        #     ReplacementTransform(relu_graph, step_graph),
-        #     FadeTransform(relu_label, step_label),
-        # )
-        # self.wait()
-
-        # parabola = axes.get_graph(lambda x: 0.25 * x**2)
-        # parabola.set_stroke(BLUE)
-        # self.play(
-        #     FadeOut(step_graph),
-        #     FadeOut(step_label),
-        #     ShowCreation(parabola)
-        # )
-        # self.wait()
-
-        # # You can use axes.input_to_graph_point, abbreviated
-        # # to axes.i2gp, to find a particular point on a graph
-        # dot = Dot(color=RED)
-        # dot.move_to(axes.i2gp(2, parabola))
-        # self.play(FadeIn(dot, scale=0.5))
-
-        # # A value tracker lets us animate a parameter, usually
-        # # with the intent of having other mobjects update based
-        # # on the parameter
-        # x_tracker = ValueTracker(2)
-        # f_always(
-        #     dot.move_to,
-        #     lambda: axes.i2gp(x_tracker.get_value(), parabola)
-        # )
-
-        # self.play(x_tracker.animate.set_value(4), run_time=3)
-        # self.play(x_tracker.animate.set_value(-2), run_time=3)
         self.wait()
 
 
