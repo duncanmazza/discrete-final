2c2ffd9156167733c538118c2b9cade22e2b7d1b
Duncan Mazza <dmazza@olin.edu>
2021-12-06 22:08:51 -0500

    Run code refactoring

--- firstGraph.py
+++ firstGraph.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 from manim import *
 from manim.utils.file_ops import open_file as open_media_file
-import numpy as np
+
 
 # To watch one of these scenes, run the following:
 # manimgl example_scenes.py OpeningManimExample
@@ -17,18 +16,17 @@ class DiscreteNewGraph(Scene):
         self.play(Write(axes, lag_ratio=0.01, run_time=1))
 
         x_2 = axes.plot(
-            lambda x: x*x,
-            color = BLUE
+            lambda x: x * x,
+            color=BLUE
         )
 
         x_4 = axes.plot(
-            lambda x: (x-1)*(x-1)*(x+1)*(x+1),
-            color = BLUE
+            lambda x: (x - 1) * (x - 1) * (x + 1) * (x + 1),
+            color=BLUE
         )
 
         x_2_label = axes.get_graph_label(x_2, "x^2")
         x_4_label = axes.get_graph_label(x_4, "(x-1)^2 (x+1)^2")
-
 
         dot = Dot(color=RED)
         dot.move_to(axes.c2p(0, 0))
@@ -36,13 +34,11 @@ class DiscreteNewGraph(Scene):
         dots = []
         dot_group = VGroup()
 
-        for i in range(-5,5):
+        for i in range(-5, 5):
             new_dot = Dot(color=RED)
-            new_dot.move_to(axes.c2p(i, i*i))
+            new_dot.move_to(axes.c2p(i, i * i))
             dot_group.add(new_dot)
             dots.append((i, new_dot))
-
-
 
         self.play(
             Create(x_2),
@@ -52,17 +48,15 @@ class DiscreteNewGraph(Scene):
 
         x_2_label.shift(RIGHT)
 
-
         dots2 = []
         dot_group2 = VGroup()
 
-        for i in range(-5,5):
+        for i in range(-5, 5):
             new_dot = Dot(color=RED)
-            new_dot.move_to(axes.c2p(i, (i-1)*(i-1)*(i+1)*(i+1)))
+            new_dot.move_to(axes.c2p(i, (i - 1) * (i - 1) * (i + 1) * (i + 1)))
             dot_group2.add(new_dot)
             dots2.append((i, new_dot))
 
-        
         self.play(
             ReplacementTransform(x_2, x_4),
             FadeOut(x_2_label),
@@ -79,22 +73,20 @@ class NewGraph(Scene):
         self.play(Write(axes, lag_ratio=0.01, run_time=1))
 
         x_2 = axes.plot(
-            lambda x: x*x,
-            color = BLUE
+            lambda x: x * x,
+            color=BLUE
         )
 
         x_4 = axes.plot(
-            lambda x: (x-1)*(x-1)*(x+1)*(x+1),
-            color = BLUE
+            lambda x: (x - 1) * (x - 1) * (x + 1) * (x + 1),
+            color=BLUE
         )
 
         x_2_label = axes.get_graph_label(x_2, "x^2")
         x_4_label = axes.get_graph_label(x_4, "(x-1)^2 (x+1)^2")
 
-        x_4_label.shift(RIGHT*5 + UP*3)
-
+        x_4_label.shift(RIGHT * 5 + UP * 3)
         # x_4_label.shift(RIGHT)
-
 
         self.play(
             Create(x_2),
