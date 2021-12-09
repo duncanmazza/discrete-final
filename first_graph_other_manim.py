d94964b0411799b3ceb759d88ed734b14c221c98
csnow <colinmsnow@gmail.com>
2021-12-09 00:24:21 -0500

    First pass lengendre transform

--- first_graph_other_manim.py
+++ first_graph_other_manim.py
@@ -9,6 +9,58 @@ import numpy as np
 # Use -o to write it to a file and open it once done
 # Use -n <number> to skip ahead to the n'th animation of a scene.
 
+# class TangentLineExample(Scene):
+#     def construct(self):
+#         circle = Circle(radius=2)
+#         line_1 = TangentLine(circle, alpha=0.0, length=4, color=BLUE_D) # right
+#         line_2 = TangentLine(circle, alpha=0.4, length=4, color=GREEN) # top left
+#         self.add(circle, line_1, line_2)
+
+
+
+
+
+class TangentLineGraph(Scene):
+    def construct(self):
+        axes = Axes((-5, 5), (-1, 5))
+        # axes.add_coordinate_labels()
+
+        self.play(Write(axes, lag_ratio=0.01, run_time=1))
+
+        x_2 = axes.get_graph(
+            lambda x: x*x,
+            color = BLUE
+        )
+
+        x_4 = axes.get_graph(
+            lambda x: (x-1)*(x-1)*(x+1)*(x+1),
+            color = BLUE
+        )
+        x_2_label = axes.get_graph_label(x_2, "x^2")
+
+        dot = Dot(color=RED)
+        dot.move_to(axes.c2p(0, 0))
+
+
+        self.play(
+            Create(x_4),
+            # FadeIn(x_2_label),
+        )
+
+        alpha = ValueTracker(.45)
+
+        draw_tangent = (lambda: 
+            TangentLine(x_4, alpha.get_value(), 5, color=WHITE))
+
+        tangent = always_redraw(draw_tangent)  
+
+        self.play(Create(tangent))
+
+        self.play(alpha.animate.set_value(.55), run_time = 10, rate_func=rate_functions.linear)
+
+        self.wait()
+
+
 class DiscreteNewGraph(Scene):
     def construct(self):
         axes = Axes((-5, 5), (-1, 5))
